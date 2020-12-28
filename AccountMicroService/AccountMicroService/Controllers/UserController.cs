855f5ad963f3bca4b625ddcf413bab0247495e0b
saidani wael <saidaniwael265@gmail.com>
2020-12-28 20:54:07 +0100

    Add Account MicroService

---
+++ AccountMicroService/AccountMicroService/Controllers/UserController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AccountMicroService.Data;
+using AccountMicroService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AccountMicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+     
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        //private readonly IUserData _userData;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ApplicationDbContext context , UserManager<IdentityUser> userManager /*, IUserData userData*/ , ILogger<UserController> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            //_userData = userData;
+            _logger = logger;
+        }
+
+            //[HttpGet]
+            //public UserModel GetById()
+            //{
+            //string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);//RequestContext.Principal.Identity.GetUserId();
+
+
+            //    return _userData.GetUserById(userId).First();
+
+            //}
+
+           [Authorize(Roles = "Admin")]
+            [HttpGet]
+            [Route("Admin/GetAllUsers")]
+
+            public List<ApplicationUserModel> GetAllUsers()
+            {
+                List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+              
+                    var users = _context.Users.ToList();
+
+            var userRoles = from ur in _context.UserRoles
+                            join r in _context.Roles on ur.RoleId equals r.Id
+                            select new { ur.UserId, ur.RoleId, r.Name };
+
+                    foreach (var user in users)
+                    {
+                        ApplicationUserModel u = new ApplicationUserModel
+                        {
+                            Id = user.Id,
+                            Email = user.Email
+                        };
+
+                u.Roles = userRoles.Where(x => x.UserId == u.Id).ToDictionary(key => key.RoleId, val => val.Name);
+                        //foreach (var r in user.Roles)
+                        //{
+                        //    u.Roles.Add(r.RoleId, roles.Where(x => x.Id == r.RoleId).First().Name);
+                        //}
+                        output.Add(u);
+
+                    }
+
+                    return output;
+                
+
+            }
+
+
+
+            [Authorize(Roles = "Admin")]
+            [HttpGet]
+            [Route("Admin/GetAllRoles")]
+
+            public Dictionary<string, string> GetAllRoles()
+            {
+               
+                    var roles = _context.Roles.ToDictionary(x => x.Id, x => x.Name);
+
+                    return roles;
+                
+            }
+
+            [Authorize(Roles = "Admin")]
+            [HttpPost]
+            [Route("Admin/AddRole")]
+
+            public async Task AddRole(UserRolePairModel pairing)
+            {
+            string loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+
+            _logger.LogInformation("Admin {Admin} added user {user} to role {Role}" , loggedInUserId , user.Id , pairing.RoleName);
+
+            await _userManager.AddToRoleAsync(user, pairing.RoleName);
+                
+
+            }
+
+            [Authorize(Roles = "Admin")]
+            [HttpPost]
+            [Route("Admin/RemoveRole")]
+
+            public async Task RemoveRole(UserRolePairModel pairing)
+            {
+            string loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+
+            _logger.LogInformation("Admin {Admin} removed user {user} from role {Role}", loggedInUserId, user.Id, pairing.RoleName);
+
+            await _userManager.RemoveFromRoleAsync(user, pairing.RoleName);
+                
+            }
+        }
+    
+}
